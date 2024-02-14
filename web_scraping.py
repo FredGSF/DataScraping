91d09f9df0b95290be8ac47a93176718e7f55b89
Frederico Fernandes <99353119+FredGSF@users.noreply.github.com>
2024-02-14 13:58:59 +0000

    Update web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -8,7 +8,7 @@ with open("index.html", "r") as f:
 tags = doc.find_all("p")[0]
 
 print(tags.find_all("b"))
-
+ 
 # HTML From Website
 url = "https://www.newegg.ca/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Description=3080&cm_re=3080-_-14-932-436-_-Product"
 
